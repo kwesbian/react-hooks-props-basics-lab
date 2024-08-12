5dfc1c7eb20fa0e1c4f80ac404cc5ecfff6f7ac1
Christian James <kwechristian@gmail.com>
2024-08-12 15:41:59 +0300

    fix issues

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home(props) {
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+        Liza is a Web Developer from New York
       </h1>
     </div>
   );
---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links() {
+    return (
+        <div id="links">
+            <h3>Links</h3>
+            <a href="https://github.com/liza">Github</a>
+            <a href="https://www.linkedin.com/in/liza/">LinkedIn</a>
+        </div>
+    );
+}
+
+export default Links;
